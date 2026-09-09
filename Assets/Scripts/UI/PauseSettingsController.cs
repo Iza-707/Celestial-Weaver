3cf6338775ff71d79a73a3363294ba001ce4924c
Iza <qafmacalalad@tip.edu.ph>
2026-09-09 15:44:58 +0800

    Added pause menu shit

---
+++ Assets/Scripts/UI/PauseSettingsController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseSettingsController : MonoBehaviour
+{
+    [Header("Settings UI")]
+    [SerializeField] private Slider masterVolumeSlider;
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+    [SerializeField] private Toggle fullscreenToggle;
+
+    private const string MasterVolumeKey = "MasterVolume";
+    private const string MusicVolumeKey = "MusicVolume";
+    private const string SFXVolumeKey = "SFXVolume";
+    private const string FullscreenKey = "Fullscreen";
+
+    private void OnEnable()
+    {
+        LoadSettings();
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetFloat(
+            MasterVolumeKey,
+            masterVolumeSlider.value
+        );
+
+        PlayerPrefs.SetFloat(
+            MusicVolumeKey,
+            musicVolumeSlider.value
+        );
+
+        PlayerPrefs.SetFloat(
+            SFXVolumeKey,
+            sfxVolumeSlider.value
+        );
+
+        PlayerPrefs.SetInt(
+            FullscreenKey,
+            fullscreenToggle.isOn ? 1 : 0
+        );
+
+        PlayerPrefs.Save();
+
+        ApplyFullscreen();
+    }
+
+    public void ApplyFullscreen()
+    {
+        Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    private void LoadSettings()
+    {
+        masterVolumeSlider.value =
+            PlayerPrefs.GetFloat(MasterVolumeKey, 1f);
+
+        musicVolumeSlider.value =
+            PlayerPrefs.GetFloat(MusicVolumeKey, 1f);
+
+        sfxVolumeSlider.value =
+            PlayerPrefs.GetFloat(SFXVolumeKey, 1f);
+
+        fullscreenToggle.isOn =
+            PlayerPrefs.GetInt(FullscreenKey, 1) == 1;
+
+        ApplyFullscreen();
+    }
+}
